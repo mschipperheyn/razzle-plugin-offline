a0fc47a4cfe273731f6fc9c89d3afd51b7d3c8d2
Marc Schipperheyn <mschipperheyn@gmail.com>
2019-01-31 11:03:54 -0200

    update version and example

--- example/src/client.js
+++ example/src/client.js
@@ -1,7 +1,7 @@
-import App from './App';
-import BrowserRouter from 'react-router-dom/BrowserRouter';
 import React from 'react';
 import { hydrate } from 'react-dom';
+import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 hydrate(
   <BrowserRouter>
